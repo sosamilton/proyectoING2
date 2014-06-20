71bac45d1006c44e5d3686fe845e23ba8019302d
xvrcjs <javierbenitezalvarenga@gmail.com>
2014-06-20 11:55:14 -0300

    Eliminar?

--- src/CB/InicioBundle/Controller/LibroController.php
+++ src/CB/InicioBundle/Controller/LibroController.php
@@ -193,7 +193,7 @@ class LibroController extends Controller
         if ($editForm->isValid()) {
             $em->flush();
 
-            return $this->redirect($this->generateUrl('libro_edit', array('id' => $id)));
+            return $this->redirect($this->generateUrl('libro'));
         }
 
         return array(
