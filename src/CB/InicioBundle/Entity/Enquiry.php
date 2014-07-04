82bbd18a2707b72d0368a15750ade10a75d7159f
Josefina <josefinadeurquiza@hotmail.com>
2014-07-04 15:05:36 -0300

    jnhsdikwsa

---
+++ src/CB/InicioBundle/Entity/Enquiry.php
@@ -0,0 +1,61 @@
+<?php
+namespace CB\InicioBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Enquiry
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Enquiry
+{
+    protected $name;
+
+    protected $email;
+
+    protected $subject;
+
+    protected $body;
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function getSubject()
+    {
+        return $this->subject;
+    }
+
+    public function setSubject($subject)
+    {
+        $this->subject = $subject;
+    }
+
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    public function setBody($body)
+    {
+        $this->body = $body;
+    }
+}
